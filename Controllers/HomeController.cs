86d6458c1c55e05e4722d695b490548d39f169a4
Armin Shoeibi <arminaspnetcore@hotmail.com>
2020-11-02 01:08:01 +0330

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JavascriptTutorial.Controllers
+{
+    /// <summary>
+    /// Javascript Tutorial
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //console Object
+        public IActionResult Lesson1() => View();
+    }
+}
